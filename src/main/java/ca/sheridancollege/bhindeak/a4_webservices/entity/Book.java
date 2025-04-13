810e9aee80aeace87a288b60d0c5a5419a2a21cd
Aksheen Bhinder <bhindeak@sheridancollege.ca>
2025-04-12 20:43:21 -0400

    Book class has been created with book id, title, auhtor name, price and quantity

---
+++ src/main/java/ca/sheridancollege/bhindeak/a4_webservices/entity/Book.java
@@ -0,0 +1,72 @@
+// Author: Aksheen, Student ID: 991762812
+package ca.sheridancollege.bhindeak.a4_webservices.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String authorName;
+    private Double price;
+    private Integer quantity;
+
+    // Default constructor
+    public Book() {
+    }
+
+    // Parameterized constructor
+    public Book(Long id, String title, String authorName, Double price, Integer quantity) {
+        this.id = id;
+        this.title = title;
+        this.authorName = authorName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
