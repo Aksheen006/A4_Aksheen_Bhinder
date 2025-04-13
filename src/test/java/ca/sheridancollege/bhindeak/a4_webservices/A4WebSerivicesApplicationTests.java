4847caff865df01a12fa716c01b2e6cca7b764f7
Aksheen Bhinder <bhindeak@sheridancollege.ca>
2025-04-12 20:35:20 -0400

    intital commit

---
+++ src/test/java/ca/sheridancollege/bhindeak/a4_webservices/A4WebSerivicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.bhindeak.a4_webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A4WebSerivicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
